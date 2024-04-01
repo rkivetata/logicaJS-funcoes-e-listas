8834f29f120378245ade6311cecd0eaadd6556a1
Thaís Silva <51369683+rkivetata@users.noreply.github.com>
2024-03-31 23:23:10 -0300

    FINALIZACAO DO CURSO

---
+++ logica de programacao js 2/desafios/desafio 3/desafio.js
@@ -0,0 +1,14 @@
+//1-Crie uma função que calcule o índice de massa corporal (IMC) de uma pessoa, a partir de sua altura, em metros, e peso, em quilogramas, que serão recebidos como parâmetro.
+
+function caluloIMC(alturaMetros, pesoKilos) {
+   let imc = pesoKilos (alturaMetros/alturaMetros);
+}
+
+//3- Crie uma função que converte um valor em dólar, passado como parâmetro, e retorna o valor equivalente em reais. Para isso, considere a cotação do dólar igual a R$ 4,80.
+
+function dolar(valorEmDolar) {
+    let cotacaoDoDolar = 4.80;
+    let valorEmReais = valorEmDolar * cotacaoDoDolar;
+    return valorEmDolar
+}
+
